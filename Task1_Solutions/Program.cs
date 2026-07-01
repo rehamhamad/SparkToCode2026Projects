3734f3ea95d67949411b9254122b11cc46c84feb
Reham <rehamxb@gmail.com>
2026-07-01 22:50:22 +0400

    Add Project Files

---
+++ Task1_Solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
