f35699f0548e862f21f4a7ddfe116688f566fa77
Reham <rehamxb@gmail.com>
2026-07-01 22:52:16 +0400

    Task 1 - Personal Info Card

--- Task1_Solutions/Program.cs
+++ Task1_Solutions/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Easy 5 Tasks 
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //Task 1: Personal Info Card 
+
+            //Declare variables to store a person's name (string), age (int), height in meters (double), and whether they are a
+            //student(bool).Assign them values directly in the code(no input needed), then print them as a single formatted info
+            //card.
+            //Requirements:
+            //• Use at least four variables with four different data types.
+            //• Print each value on its own line with a clear label.
+            //Sample output: Name: Sara, Age: 21, Height: 1.65, Student: True
+
+            Console.WriteLine("Task 1 : Personal Info Card");
+            string name = "Reham";
+            int age = 22;
+            double hight = 1.54;
+            bool isStudent = true;
+
+            Console.WriteLine("Name:" + name + ", Age:" + age + ", Hight:" + hight + ", Student:" + isStudent + "\n");
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
     }
 }
