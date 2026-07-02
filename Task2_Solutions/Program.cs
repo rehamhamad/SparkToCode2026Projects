9d356802f5f200d667de13fac50eac6b2586ab2e
Reham <rehamxb@gmail.com>
2026-07-02 11:48:37 +0400

    Task 1 - Countdown Timer

--- Task2_Solutions/Program.cs
+++ Task2_Solutions/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //            Task 1 - Countdown Timer
+            //Ask the user to enter a starting number, then print a countdown from that number down to 1 using a for loop, and
+            //print "Liftoff!" after the loop ends.
+            //Requirements:
+            //• Use a for loop that decreases the counter on each iteration.
+            //• Print one number per line during the countdown.
+
+            Console.Write("Enter you starting number for the countdown: ");
+            int start = int.Parse(Console.ReadLine());
+            for (int i = start; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
+
+            ///////////////////////////////////////////////////////////////////
+            
+
+
+
         }
     }
 }
