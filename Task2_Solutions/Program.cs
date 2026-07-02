0a84c98bf0dc3bc5ceda30870236004c2023b005
Reham <rehamxb@gmail.com>
2026-07-02 11:57:40 +0400

    Task 2 - Sum of Numbers 1 to N

--- Task2_Solutions/Program.cs
+++ Task2_Solutions/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Task2_Solutions
+﻿using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task2_Solutions
 {
     internal class Program
     {
@@ -20,7 +26,26 @@
             Console.WriteLine("Liftoff!");
 
             ///////////////////////////////////////////////////////////////////
-            
+
+            //            Task 2 - Sum of Numbers 1 to N
+            //Ask the user to enter a positive whole number N, then use a for loop to calculate the sum of all whole numbers from
+            //1 to N, and print the final sum.
+            //Requirements:
+            //• Use a variable initialized to 0 to accumulate the total inside the loop.
+            //• Print the final sum only once, after the loop has finished.
+
+
+            Console.Write("Enter a positive whole number: ");
+            int N = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1 ; i <= N; i++) {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of all whole numbers from 1 to" + N + " is "+ sum );
+
+            ///////////////////////////////////////////////////////////////////
+
+
 
 
 
