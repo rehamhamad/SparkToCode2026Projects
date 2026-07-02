ab2fc60f0e2ffdf3933fa2dc7b8377542c3f4394
Reham <rehamxb@gmail.com>
2026-07-02 11:47:03 +0400

    Add Task 2 file

---
+++ Task2_Solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task2_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
