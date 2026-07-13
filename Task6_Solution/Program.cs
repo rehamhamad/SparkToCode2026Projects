d39377c7ba3ba240ea9838051d81907713f46df9
Reham <rehamxb@gmail.com>
2026-07-14 01:13:43 +0400

    create bank account class

---
+++ Task6_Solution/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task6_Solution
+{
+    internal class Program
+    {
+
+        //bankaccount class
+        class BankAccount
+        {
+            public int AccountNumber;
+            public string HolderName;
+            public double Balance;
+
+            public void Deposit(double amount)
+            {
+                Balance += amount;
+                SendEmail();
+            }
+
+            public void Withdraw(double amount)
+            {
+                if (Balance >= amount)
+                {
+                    Balance -= amount;
+                    SendEmail();
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient balance.");
+                }
+            }
+
+            public double CheckBalance()
+            {
+                PrintInformation();
+                return Balance;
+            }
+
+            private void PrintInformation()
+            {
+                Console.WriteLine($"Account Holder: {HolderName}");
+                Console.WriteLine($"Balance: {Balance}");
+            }
+
+            private void SendEmail()
+            {
+                Console.WriteLine("Email notification sent.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
